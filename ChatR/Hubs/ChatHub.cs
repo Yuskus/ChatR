798a53de33306fd5567f284d484cf6971029616a
Yuskus <yuskus@yandex.ru>
2026-04-06 09:27:22 +0300

    fix logs

--- ChatR/Hubs/ChatHub.cs
+++ ChatR/Hubs/ChatHub.cs
@@ -27,7 +27,6 @@ namespace ChatR.Hubs
 
         public async Task SendMessage(string content, int userId, int roomId)
         {
-            Console.WriteLine("1 - " + Context.User?.Identity?.IsAuthenticated.ToString());
             var message = await _messageService.Add(content, userId, roomId);
 
             if (message != null)
